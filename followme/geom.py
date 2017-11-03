6d784ab7440390d359f05106790738370bf6f29b
Lars Kellogg-Stedman <lars@redhat.com>
2017-11-03 13:18:03 -0400

    better handling of n-dimensional points

--- followme/geom.py
+++ followme/geom.py
@@ -6,18 +6,19 @@ import math
 
 LOG = logging.getLogger(__name__)
 
-_Point = collections.namedtuple('_Point', ['lat', 'lon'])
+_Point = collections.namedtuple('Point', ['lat', 'lng'])
+_Point3D = collections.namedtuple('Point3D', ['lat', 'lng', 'alt'])
+_Point4D = collections.namedtuple('Point4D', ['lat', 'lng', 'alt', 'bearing'])
 
 # radius of the earth
 R = 6371e3
 
 
-class Point(_Point):
-    '''A geographic point represented as a (lat, lon) tuple.'''
+class PointBase(object):
 
     def to_radians(self):
         '''Convert the latitude and longitude to radians.'''
-        return _Point(math.radians(self.lat), math.radians(self.lon))
+        return _Point(math.radians(self.lat), math.radians(self.lng))
 
     def distance_to(self, them):
         '''Calculate the distance to another Point.
@@ -31,12 +32,12 @@ class Point(_Point):
         self_r = self.to_radians()
         them_r = them.to_radians()
         dlat = them_r.lat - self_r.lat
-        dlon = them_r.lon - self_r.lon
+        dlng = them_r.lng - self_r.lng
 
         a = (
             (math.sin(dlat/2) * math.sin(dlat/2)) +
             (math.cos(self_r.lat) * math.cos(them_r.lat) *
-             math.sin(dlon/2) * math.sin(dlon/2))
+             math.sin(dlng/2) * math.sin(dlng/2))
         )
 
         c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
@@ -58,11 +59,11 @@ class Point(_Point):
         self_r = self.to_radians()
         them_r = them.to_radians()
 
-        dlon = them_r.lon - self_r.lon
-        x = math.sin(dlon) * math.cos(them_r.lat)
+        dlng = them_r.lng - self_r.lng
+        x = math.sin(dlng) * math.cos(them_r.lat)
         y = (
                 (math.cos(self_r.lat) * math.sin(them_r.lat)) -
-                (math.sin(self_r.lat) * math.cos(them_r.lat) * math.cos(dlon))
+                (math.sin(self_r.lat) * math.cos(them_r.lat) * math.cos(dlng))
                 )
 
         theta = math.atan2(x, y)
@@ -73,19 +74,32 @@ class Point(_Point):
         b = math.radians(b)
 
         lat1 = self_r.lat
-        lon1 = self_r.lon
+        lng1 = self_r.lng
 
         lat2 = math.asin(
             (math.sin(lat1) * math.cos(d/R)) +
             (math.cos(lat1) * math.sin(d/R) * math.cos(b))
         )
 
-        lon2 = lon1 + math.atan2(
+        lng2 = lng1 + math.atan2(
             (math.sin(b) * math.sin(d/R) * math.cos(lat1)),
             (math.cos(d/R) - math.sin(lat1) * math.sin(lat2))
         )
 
-        return Point(
+        return self.__class__(
             math.degrees(lat2),
-            math.degrees(lon2),
+            math.degrees(lng2),
+            *self[2:]
         )
+
+
+class Point(_Point, PointBase):
+    '''A geographic point represented as a (lat, lng) tuple.'''
+
+
+class Point3D(_Point3D, PointBase):
+    pass
+
+
+class Point4D(_Point4D, PointBase):
+    pass
--- tests/test_geom.py
+++ tests/test_geom.py
@@ -1,12 +1,16 @@
 import unittest
 
-from followme.geom import Point
+from followme.geom import Point, Point3D, Point4D  # NOQA
 
 # whitehouse
 p1 = Point(38.897741, -77.036450)
+p1z = Point3D(38.897741, -77.036450, 10)
+p1zb = Point4D(38.897741, -77.036450, 10, 180)
 
 # pool (180 meters due south of the whitehouse)
 p2 = Point(38.89612222110935, -77.03645)
+p2z = Point3D(38.89612222110935, -77.03645, 10)
+p2zb = Point4D(38.89612222110935, -77.03645, 10, 180)
 
 
 class TestPoint(unittest.TestCase):
@@ -25,3 +29,17 @@ class TestPoint(unittest.TestCase):
         assert abs(180 - d1) < 1
         d2 = p2.distance_to(p3)
         assert d2 < 1
+
+    def test_2d_3d_compat(self):
+        d = p1z.distance_to(p2)
+        assert abs(180 - d) < 1
+
+        d = p1.distance_to(p2z)
+        assert abs(180 - d) < 1
+
+    def test_2d_4d_compat(self):
+        d = p1zb.distance_to(p2)
+        assert abs(180 - d) < 1
+
+        d = p1.distance_to(p2zb)
+        assert abs(180 - d) < 1
